12424daf1c70b1a001a697c143814823e02c8703
Jaehyeon Park <skystar@skystar.dev>
2025-11-08 16:42:21 +0900

    add create_owned_deployment skeleton

--- src/controller.rs
+++ src/controller.rs
@@ -1,8 +1,12 @@
 use std::{sync::Arc, time::Duration};
 
-use kube::{ResourceExt, runtime::controller::Action};
+use k8s_openapi::api::apps::v1::Deployment;
+use kube::{Resource, ResourceExt, api::ObjectMeta, runtime::controller::Action};
 
-use crate::{crd::MultiDeployment, types::{Context, Error}};
+use crate::{
+    crd::{ChildDeployment, MultiDeployment},
+    types::{Context, Error},
+};
 
 pub async fn reconcile(obj: Arc<MultiDeployment>, _ctx: Arc<Context>) -> Result<Action, Error> {
     println!("Reconciling Pod: {}", obj.name_any());
@@ -11,4 +15,25 @@ pub async fn reconcile(obj: Arc<MultiDeployment>, _ctx: Arc<Context>) -> Result<
 
 pub fn error_policy(_obj: Arc<MultiDeployment>, _error: &Error, _ctx: Arc<Context>) -> Action {
     Action::requeue(Duration::from_secs(5))
+}
+
+fn create_owned_deployment(source: &MultiDeployment, child_name: String) -> Result<Deployment, Error> {
+    let oref = source.controller_owner_ref(&()).unwrap();
+    let source_name = source.name_any();
+    let child_deployment = source.spec.children.get(&child_name).unwrap();
+
+    let deployment = Deployment {
+        metadata: ObjectMeta {
+            name: Some(format!("{}-{}", source_name, child_name)),
+            owner_references: Some(vec![oref]),
+            ..Default::default()
+        },
+
+        ..Default::default()
+    };
+
+    // 1. Get spec from root_template
+    // 2. Merge with spec from child_deployment
+
+    todo!()
 }
--- src/crd.rs
+++ src/crd.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 use k8s_openapi::{
-    api::apps::v1::{Deployment, DeploymentSpec},
+    api::apps::v1::DeploymentSpec,
     serde::{Deserialize, Serialize},
 };
-use kube::{CustomResource, Resource, api::ObjectMeta};
+use kube::{CustomResource, api::ObjectMeta};
 use schemars::JsonSchema;
 
 #[derive(CustomResource, Deserialize, Serialize, Clone, Debug, JsonSchema)]
@@ -16,19 +16,20 @@ use schemars::JsonSchema;
 )]
 #[kube(status = "MultiDeploymentStatus")]
 pub struct MultiDeploymentSpec {
-    name: String,
-    root_template: DeploymentSpec,
-    children: HashMap<String, PartialDeployment>,
+    pub name: String,
+    pub replicas: Option<i32>,
 
-    replicas: Option<i32>,
+    pub root_template: DeploymentSpec,
+    pub children: HashMap<String, ChildDeployment>,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, JsonSchema)]
 pub struct MultiDeploymentStatus {}
 
-#[derive(Resource, Serialize, Deserialize, Debug, Clone, JsonSchema)]
-#[resource(inherit = Deployment)]
-pub struct PartialDeployment {
-    metadata: ObjectMeta,
-    weight: Option<i32>,
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct ChildDeployment {
+    pub weight: Option<i32>,
+    pub min_replicas: Option<i32>,
+
+    pub spec: DeploymentSpec,
 }
